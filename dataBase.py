3bbeb7a86bac9cb320d94f8be6cc48a8e74542b0
gosha-krovsh <51998380+gosha-krovsh@users.noreply.github.com>
2020-07-18 11:22:28 +0300

    dataBase test

--- dataBase.py
+++ dataBase.py
@@ -1,1 +1,14 @@
-import pymysql
+import mysql.connector as connector
+
+db = connector.connect(
+    host = "localhost",
+    user = "gosha_krovsh",
+    passwd = "",
+    database = "MapaApp"
+)
+
+cursor = db.cursor()
+cursor.execute('SELECT name FROM Towns')
+print(cursor.next())
+
+db.close()
